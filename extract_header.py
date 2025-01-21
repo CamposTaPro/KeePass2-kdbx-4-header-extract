3f0fd09defc6352e2b6f0f856eca228cf82fddce
Miguel Campos <filipe422@gmail.com>
2025-01-21 18:36:15 +0000

    code cleanup

--- extract_header.py
+++ extract_header.py
@@ -27,71 +27,45 @@ def read_fixed_size_string(file,size):
 def read_varientMap(file):
 
     varientDictionaryFormat=read_fixed_size(file,2,"<H")
-    #print(hex(varientDictionaryFormat))
     uuidKDFType= file.read(1) # not used for anything but it still needs to be read
     uuidKDFNameSize=read_fixed_size(file,4,"<I")
-    #print(uuidKDFNameSize)
     uuidKDFName=read_fixed_size_string(file,uuidKDFNameSize)
-    #print(uuidKDFName)
     uuidKDFValueSize=read_fixed_size(file,4,"<I")
-    #print(uuidKDFValueSize)
     uuidKDFValue=read_fixed_size(file,uuidKDFValueSize,None,True)
-    #print(uuidKDFValue)
     if uuidKDFValue in ["ef636ddf8c29444b91f7a9a403e30a0c","9e298b1956db4773b23dfc3ec6f0a1e6"]:
         kdf=1             #this variable is only to help distiguish the KDFs -> 1 - argon2d or argon2id and 2 - AES-KDF
         ######## For entry I (Iterations)  ##################
         argon2IterationType= file.read(1)
         argon2IterationNameSize=read_fixed_size(file,4,"<I")
-        #print(argon2IterationNameSize)
         argon2IterationName=read_fixed_size_string(file,argon2IterationNameSize)
-        #print(argon2IterationName)
         argon2IterationValueSize=read_fixed_size(file,4,"<I")
-        #print(argon2IterationValueSize)
         argon2IterationValue=read_fixed_size(file,argon2IterationValueSize,"<Q")
-        #print(argon2IterationValue)
         ######## For entry M (Memory)  ##################
         argon2MemoryType= file.read(1)
         argon2MemoryNameSize=read_fixed_size(file,4,"<I")
-        #print(argon2MemoryNameSize)
         argon2MemoryName=read_fixed_size_string(file,argon2MemoryNameSize)
-        #print(argon2MemoryName)
         argon2MemoryValueSize=read_fixed_size(file,4,"<I")
-        #print(argon2MemoryValueSize)
         argon2MemoryValue=read_fixed_size(file,argon2MemoryValueSize,"<Q")
-        #print(argon2MemoryValue)
         ######## For entry P (Parallelism)  ##################
         argon2ParallelismTypeByte= file.read(1)
         argon2ParallelismNameSize=read_fixed_size(file,4,"<I")
-        #print(argon2ParallelismNameSize)
         argon2ParallelismName=read_fixed_size_string(file,argon2ParallelismNameSize)
-        #print(argon2ParallelismName)
         argon2ParallelismValueSize=read_fixed_size(file,4,"<I")
-        #print(argon2ParallelismValueSize)
         argon2ParallelismValue=read_fixed_size(file,argon2ParallelismValueSize,"<I")
-        #print(argon2ParallelismValue)
         ######## For entry S (Salt)  ##################
         argon2SaltTypeByte= file.read(1)
         argon2SaltNameSize=read_fixed_size(file,4,"<I")
-        #print(argon2SaltNameSize)
         argon2SaltName=read_fixed_size_string(file,argon2SaltNameSize)
-        #print(argon2SaltName)
         argon2SaltValueSize=read_fixed_size(file,4,"<I")
-        #print(argon2SaltValueSize)
         argon2SaltValue=read_fixed_size(file,argon2SaltValueSize,None,True)
-        #print(argon2SaltValue)
         ######## For entry V (Version)  ##################
         argon2VersionTypeByte= file.read(1)
         argon2VersionNameSize=read_fixed_size(file,4,"<I")
-        #print(argon2VersionNameSize)
         argon2VersionName=read_fixed_size_string(file,argon2VersionNameSize)
-        #print(argon2VersionName)
         argon2VersionValueSize=read_fixed_size(file,4,"<I")
-        #print(argon2VersionValueSize)
         argon2VersionValue=read_fixed_size(file,argon2VersionValueSize,"<I",True)
-        #print(hex(argon2VersionValue))
         ######## Check for end of VariantMap ##################
         checkEnd=read_fixed_size(file,1,None,True)
-        #print(checkEnd)
         data={
             "varientDictionaryFormat": hex(varientDictionaryFormat),
             "uuidKDFValueSize":uuidKDFValueSize,
@@ -122,23 +96,16 @@ def read_varientMap(file):
         aesRoundsType= file.read(1)
         aesRoundsNameSize=read_fixed_size(file,4,"<I")
         aesRoundsName=read_fixed_size_string(file,aesRoundsNameSize)
-        #print(aesRoundsName)
         aesRoundsValueSize=read_fixed_size(file,4,"<I")
-        #print(aesRoundsValueSize)
         aesRoundsValue=read_fixed_size(file,aesRoundsValueSize,"<Q")
-        #print(aesRoundsValue)
         ######## For entry S (Salt/seed)  ##################
         aesSaltType= file.read(1)
         aesSaltNameSize=read_fixed_size(file,4,"<I")
         aesSaltName=read_fixed_size_string(file,aesSaltNameSize)
-        #print(aesSaltName)
         aesSaltValueSize=read_fixed_size(file,4,"<I")
-        #print(aesSaltValueSize)
         aesSaltValue=read_fixed_size(file,aesSaltValueSize,None,True)
-        #print(aesSaltValue)
         ######## Check for end of VariantMap ##################
         checkEnd=read_fixed_size(file,1,None,True)
-        #print(checkEnd)
         data={
             "varientDictionaryFormat": hex(varientDictionaryFormat),
             "uuidKDFValueSize":uuidKDFValueSize,
@@ -163,9 +130,7 @@ def readFile(path_file):
     with open(str(path_file), "rb") as file: #teste123.kdbx - AES  Passwords.kdbx- argon2d
         ######## Read Signtures ##################
         signature1=read_fixed_size(file,4,"<I")
-        #print(hex(signature1))
         signature2=read_fixed_size(file,4,"<I")
-        #print(hex(signature2))
         signature={
             "signature1":hex(signature1),
             "signature2":hex(signature2)
@@ -175,64 +140,45 @@ def readFile(path_file):
         major_version = read_fixed_size(file, 2, "<H")
         version = f"{major_version}.{minor_version}"
 
-        #print(version)
         ######## Read the ID of the algorithm used to cipher ##################
         CipherIDFlag=read_fixed_size(file,1,"<B")
-        #print(CipherIDFlag)
         CipherIDSize=read_fixed_size(file,4,"<I")
-        #print(CipherIDSize)
         CipherID=read_fixed_size(file,CipherIDSize,None,True)# important usar o .hex() quando so quero extrair o valor hexadecimal
-        #print(CipherID)
         cipher={
             "CipherIDFlag":CipherIDFlag,
             "CipherIDSize": CipherIDSize,
             "CipherID": CipherID
         }
-        #print(cipher)
         ######## Read the compression algorithm used ##################
         compressionFlag=read_fixed_size(file,1,"<B")
-        #print(compressionFlag)
         compressionSize=read_fixed_size(file,4,"<I")
-        #print(compressionSize)
         compression=read_fixed_size(file,compressionSize,"<I")
-        #print(compression)
         compressionAlgorithm={
             "compressionFlag":compressionFlag,
             "compressionSize":compressionSize,
             "compression":compression
         }
-        #print(compressionAlgorithm)
         ######## Read the salt used for computing the keys ##################
         saltFlag=read_fixed_size(file,1,"<B")
-        #print(saltFlag)
         saltSize=read_fixed_size(file,4,"<I")
-        #print(saltSize)
         salt=read_fixed_size(file,saltSize,None,True)
-        #print(salt)
         saltData={
             "saltFlag":saltFlag,
             "saltSize":saltSize,
             "salt":salt
         }
-        #print(saltData)
         ######## Read the initialization vector for the encryption algorithm. ##################
         encryptionIVFlag=read_fixed_size(file,1,"<B")
-        #print(encryptionIVFlag)
         encryptionIVSize=read_fixed_size(file,4,"<I")
-        #print(encryptionIVSize)
         encryptionIV=read_fixed_size(file,encryptionIVSize,None,True)# means the encryption algorithm is AES meaning this field is 16 bytes long
-        #print(encryptionIV)
         encryptionInitializationVector={
             "encryptionIVFlag":encryptionIVFlag,
             "encryptionIVSize":encryptionIVSize,
             "encryptionIV":encryptionIV
         }
-        #print(encryptionInitializationVector)
         ######## Read the parameters for the key derivation function (KDF) ##################
         kdfParamFlag=read_fixed_size(file,1,"<B")
-        #print(kdfParamFlag)
         kdfParamSize=read_fixed_size(file,4,"<I")
-        #print(kdfParamSize)
         kdfParameters={
             "kdfParamFlag":kdfParamFlag,
             "kdfParamSize":kdfParamSize
@@ -242,11 +188,8 @@ def readFile(path_file):
         varientMapData=read_varientMap(file)
         ######## Check for end of Headers ##################
         headerEndFlag=read_fixed_size(file,1,"<B")
-        #print(headerEndFlag)
         headerEndSize=read_fixed_size(file,4,"<I")
-        #print(headerEndSize)
         headerEnd=read_fixed_size(file,headerEndSize,"<I")
-        #print(hex(headerEnd))
     data={
         "signature":signature,
         "version":version,
